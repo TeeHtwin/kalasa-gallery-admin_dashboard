2703f2808ba1c9fbba7ed1300f7ed04e72684dc7
khantlinnyan <khantlinnyan011027@gmail.com>
2024-01-28 15:25:18 +0630

    build: Input

--- src/app/(dashboard)/artworks/page.tsx
+++ src/app/(dashboard)/artworks/page.tsx
@@ -1,19 +1,4 @@
-import Input from '@/components/common/Input';
-
 const ArtWork = () => {
-  return (
-    <div>
-      <Input
-        htmlFor="name-input"
-        name="name"
-        title="Name"
-        inputAttribute={{
-          type: 'text',
-          placeholder: 'Enter your name',
-        }}
-        size="large"
-      />
-    </div>
-  );
+  return <div>Artwork</div>;
 };
 export default ArtWork;
