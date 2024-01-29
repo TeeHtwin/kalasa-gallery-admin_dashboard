d753571c940e83ae9a2ce9701a64dccdc8bb7353
HeinHtetZaw <hzawnineseven@gmail.com>
2024-01-29 13:49:04 +0630

    Dialog_Box

--- src/app/(dashboard)/artworks/page.tsx
+++ src/app/(dashboard)/artworks/page.tsx
@@ -1,4 +1,16 @@
+import Alert from '@/components/dialog/AlertDialog';
+
 const ArtWork = () => {
-  return <div>ArtWork</div>;
+  return (
+    <div>
+      <Alert
+        trigger="Delete an artwork"
+        title=" Are you sure do you want to delete this artwork from your
+        database?"
+        description="Please make sure to check the artwork before deleting it. Because it
+        will be wiped out permanently and there’s no way to recover it back."
+      />
+    </div>
+  );
 };
 export default ArtWork;
