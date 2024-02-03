7817326690b7e34a5f571a38fa6b02fe5b79cf85
Yin Yin Kyaw <yinyinkyaw.zotefamily@gmail.com>
2024-02-03 15:27:45 +0630

    [install] packages

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,10 @@
+type Artist = {
+  id: number;
+  name: string;
+  profile_image: string;
+  description: string;
+  status: boolean;
+  total_artwork: number;
+  sold_artwork: number;
+  added_date: string;
+};
